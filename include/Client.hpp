646fc46a4d8044808a4ce7db8e06967857e6b492
ltressen <paloouf@gmail.com>
2023-12-12 16:23:30 +0100

    test

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,16 +6,27 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:57:18 by ltressen          #+#    #+#             */
-/*   Updated: 2023/12/12 11:00:39 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:06:46 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "main.hpp"
+class Server;
 
 class Client{
 	public:
-		Client();
+		Client(Server* server, int fd, std::string hostname, int port);
 		~Client();
+
+		//GETTERS
+		int	getFd() const{return _fd;}
+		std::string const &	getHostname() const{return _hostname;}
+		int	getPort() const {return _port;}
+	private:
+		int	_fd;
+		std::string _hostname;
+		int	_port;	
+		Server* server;
 };
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,15 +6,17 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:55:27 by ltressen          #+#    #+#             */
-/*   Updated: 2023/12/12 10:59:38 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:06:12 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Client.hpp"
 
-Client::Client(){
-	std::cout << "Test client\n";
+Client::Client(Server *server, int fd, std::string hostname, int port) :_fd(fd), _hostname(hostname), _port(port){
+	std::cout << "Test client, fd: " << this->getFd() << ", hostname: " << this->getHostname() << std::flush;
+	std::cout << ", port: " << this->getPort() << std::endl;	
 }
+
 Client::~Client(){
 	std::cout << "Client dead\n";
 }
