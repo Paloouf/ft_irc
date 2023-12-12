fea28641105c66502c118962b03636e53b9ec3e4
ltressen <paloouf@gmail.com>
2023-12-12 15:32:03 +0100

    ignore this

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,11 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:29:08 by ltressen          #+#    #+#             */
-/*   Updated: 2023/12/12 12:08:32 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/12/12 15:10:25 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/main.hpp"
 
 int main(){
-	Client cl;
-	Server srv;
-	std::cout << "Firstl" << std::endl;
-	return 0;
 }
