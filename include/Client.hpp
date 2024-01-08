8a80a485172bb38a9c7160993a2209ea67b44782
Jordan Casadessus <jcasades@c2r7p4.42perpignan.fr>
2024-01-08 13:29:55 +0100

    new getters and setters

--- include/Client.hpp
+++ include/Client.hpp
@@ -14,16 +14,21 @@ class Client{
 		int	getFd() const{return _fd;}
 		std::string const &	getHostname() const{return _hostname;}
 		int	getPort() const {return _port;}
+		std::string getNick() const {return _user;}
+		std::string getHost() const {return _hostname;}
 
 		//SETTERS
 		void	setIP(const std::string & ipClient){this->_ipClient = ipClient;}
 		void	setNick(const std::string nick){this->_nick = nick;}
+		void	setUser(const std::string user){this->_user = user;}
+		void	setHost(const std::string hostname){this->_hostname = hostname;}
 	private:
 		Server* _server;
 		int	_fd;
 		std::string _hostname;
 		int	_port;	
 		std::string _ipClient;
+		std::string _user;
 		std::string _nick;
 		std::vector<Channel*> _chan;
 		
